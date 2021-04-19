993845f40c958c010ff93a9940063ce15db6a180
alek101 <47416711+alek101@users.noreply.github.com>
2021-04-19 15:29:48 +0200

    my solution
    to log out challenge

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,9 +6,26 @@
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+import { watch } from 'vue'
 
 export default {
   components: { Navbar },
+  setup() {
+    let { user } = getUser()
+    const router = useRouter()
+
+    watch(user, (user) => {
+      console.log('User inside watch have changed: ',user)
+      if(!user)
+      {
+        router.push({name: 'Welcome'})
+      }
+    })
+    
+    
+  }
 
 }
 </script>
