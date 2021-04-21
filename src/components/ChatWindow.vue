a2f65e754fb8927f25a8c09f74f5c5538fb6c232
alek101 <47416711+alek101@users.noreply.github.com>
2021-04-21 11:11:27 +0200

    auto scroll

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="chat-window">
         <div v-if="error"> {{ error }}</div>
-        <div v-if="documents" class="messages">
+        <div v-if="documents" class="messages"  ref="messages">
             <div class="single" v-for="doc in formattedDocuments" :key="doc.id">
                 <span class="created-at">{{doc.createdAt}}</span>
                 <span class="name">{{doc.name}}</span>
@@ -15,11 +15,12 @@
 <script>
 import getCollection from '../composables/getCollection'
 import { formatDistanceToNow} from 'date-fns'
-import { computed } from 'vue'
+import { ref, computed, onUpdated } from 'vue'
 
 export default {
     setup() {
         const {error, documents} = getCollection('messages')
+        
 
         const formattedDocuments = computed(() => {
             if(documents.value) {
@@ -30,7 +31,14 @@ export default {
             }
         })
 
-        return {error, documents, formattedDocuments}
+        //auto-scroll
+        const messages = ref(null) 
+
+        onUpdated(()=>{
+           messages.value.scrollTop = messages.value.scrollHeight
+        })
+
+        return {error, documents, formattedDocuments, messages}
     }
 }
 </script>
